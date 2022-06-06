6c5fe5cc6c768a18eb57f901164df556d55d0e48
Nick Kuria <nkuria01@gmail.com>
2022-06-06 10:27:00 +0300

    Added DB configuration to startup class

--- store/Startup.cs
+++ store/Startup.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using store.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace store
 {
@@ -14,9 +17,20 @@ namespace store
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+       public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+        private IConfiguration Configuration { get; set; }
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<StoreDbContext>(opts =>
+            {
+                opts.UseSqlServer(
+                    Configuration["ConnectionStrings: storeConnection"]
+                    );
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
