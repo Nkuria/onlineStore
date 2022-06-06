3ad46716228b784e197b89fd6b95884bd134208d
Nick Kuria <nkuria01@gmail.com>
2022-06-06 10:13:14 +0300

    Added the db context class

---
+++ store/Models/StoreDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace store.Models
+
+{
+    public class StoreDbContext : DbContext
+    {
+        public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
