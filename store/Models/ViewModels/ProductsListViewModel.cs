e02b61c784ae6276548860beb7a41ad9cb35fcc3
Nick Kuria <nkuria01@gmail.com>
2022-06-07 06:49:20 +0300

    Added classfile ProductsListViewModel

---
+++ store/Models/ViewModels/ProductsListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using store.Models;
+namespace store.Models.ViewModels
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+
+        public PagingInfo PagingInfo { get; set; }
+
+        
+    }
+}
