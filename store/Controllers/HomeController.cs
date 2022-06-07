c53ae4b8cef805be59332cc0230fd36fe2d11669
Nick Kuria <nkuria01@gmail.com>
2022-06-07 06:07:34 +0300

    Added pagination to controller

--- store/Controllers/HomeController.cs
+++ store/Controllers/HomeController.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using store.Models;
+using System.Linq;
 namespace store.Controllers
+
 {
     public class HomeController : Controller
     {
         private IStoreRepository repository;
+        public int pageSize = 4;
 
         public HomeController(IStoreRepository repo)
         {
@@ -12,6 +15,11 @@ namespace store.Controllers
         }
 
 
-        public IActionResult index() => View(repository.Products);
+        public IActionResult index(int productPage = 1)
+         
+           => View(repository.Products.OrderBy(p => p.ProductID)
+                .Skip((productPage - 1) * pageSize)
+                .Take(pageSize)); 
+        
     }
 }
